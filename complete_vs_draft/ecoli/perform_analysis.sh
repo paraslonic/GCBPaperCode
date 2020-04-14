e514a15dbfdb3c3718b2ab4924ddf6d2c26cc806
Александр Манолов <manolov@pyxis.ripcm.com>
2020-04-14 22:14:12 +0300

    first plot, not very similar

--- complete_vs_draft/ecoli/perform_analysis.sh
+++ complete_vs_draft/ecoli/perform_analysis.sh
@@ -1,3 +1,4 @@
-python3 gcb/parse_og.py -i draft/Orthogroups.txt -o draft/draft
-python3 gcb/parse_og.py -i complete/Orthogroups.txt -o complete/complete
-python3 gcb/estimate_complexity.py -i draft/draft.sif -o draft --reference GCF_000005845.2_ASM584v2_genomic
+#python3 gcb/parse_og.py -i draft/Orthogroups.txt -o draft/draft
+#python3 gcb/parse_og.py -i complete/Orthogroups.txt -o complete/complete
+python3 gcb/estimate_complexity.py -i draft/draft.sif -o draft --reference GCF_000005845.2_ASM584v2_genomic --save_db draft/draft.db
+python3 gcb/estimate_complexity.py -i complete/complete.sif -o complete --reference GCF_000005845.2_ASM584v2_genomic --save_db complete/complete.db
